6be8111b6831f980a34340db7671d67b0b34a244
KayongoYongo <sknsammie10@gmail.com>
2023-07-24 16:53:19 +0000

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,8 +5,8 @@
 def index_range(page: int, page_size: int) -> tuple:
     # Calculate the start index
     start_index = (page - 1) * page_size
-    
+
     # Calculate the end index
     end_index = page * page_size
-    
+
     return start_index, end_index
